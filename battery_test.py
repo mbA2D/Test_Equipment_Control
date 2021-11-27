0739792fd9713526278858ab4bb708bd4e013c92
mbA2D <ottawaelectronics2016@gmail.com>
2021-11-26 22:31:16 -0800

    Updated to work with multiple processes

--- battery_test.py
+++ battery_test.py
@@ -1,60 +1,28 @@
-#Program to run the charge discharge control in different threads.
+#Program to run the charge discharge control in different processes for each channel.
 
-'''
-#Tkinter had some weird interaction with EasyGui (which is based on TKinter). After closing a window
-#in the thread, the main window would lock up.
-
-import tkinter as tk
-import threading
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton
 import charge_discharge_control as cdc
+from multiprocessing import Process
 
-def batt_test_thread():
-	thread1 = threading.Thread(target=cdc.charge_discharge_control, args = (), daemon = True)
-	thread1.start()
-
-window = tk.Tk()
-window.title('Battery Test')
-
-frm_1 = tk.Frame(master = window, relief = tk.GROOVE, borderwidth = 5)
-
-lbl_1 = tk.Label(master = frm_1, text = "Testing Application")
-lbl_1.pack(pady = 20)
-
-#btn_1 = tk.Button(master = frm_1, text = "Start Test", command = cdc.charge_discharge_control())
-btn_1 = tk.Button(master = frm_1, text = "Start Test", command = batt_test_thread)
-btn_1.pack(pady = 20)
-
-frm_1.pack()
-
-window.mainloop()
-'''
-
-
-from PyQt6.QtGui import *
-from PyQt6.QtWidgets import *
-from PyQt6.QtCore import *
-import sys
-
-import charge_discharge_control as cdc
-
-class CDC_Class(QThread):
+class MainTestWindow(QMainWindow):
 	def __init__(self):
-		QThread.__init__(self)
-	
-	def __del__(self):
-		self.wait()
+		super().__init__()
 		
-	def run(self):
-		cdc.charge_discharge_control()
-
+		self.setWindowTitle("Battery Tester App")
+		button = QPushButton("Start Test")
+		button.setCheckable(False)
+		button.clicked.connect(self.batt_test_process)
+		self.setCentralWidget(button)
+	
+	def batt_test_process():
+		process1 = Process(target=cdc.charge_discharge_control)
+		process1.start()
 
 def main():
-	app = QtGui.QApplication(sys.argv)
-	test_window = 
+	app = QApplication([])
+	test_window = MainTestWindow()
 	test_window.show()
-	app.exec_()
+	app.exec()
 	
-
 if __name__ == '__main__':
 	main()
-	
