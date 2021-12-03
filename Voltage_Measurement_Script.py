6c44257cdc4ff865432364af0936b93eeb55c79b
mbA2D <ottawaelectronics2016@gmail.com>
2021-12-03 14:07:17 -0800

    Quick Script to measure voltages

---
+++ Voltage_Measurement_Script.py
@@ -0,0 +1,20 @@
+#Quick and dirty python script to read a voltage a bunch of times
+
+import equipment as eq
+import time
+import easygui as eg
+
+dmm = eq.dmms.choose_dmm()[1]
+
+msg = "How Many Measurements to perform?"
+title = "Number of Measurements"
+num_measurements = int(eg.enterbox(msg = msg, title = title, default = 100, strip = True))
+
+msg = "How many seconds to delay before starting to measure?"
+title = "Pre-Measurement Delay"
+delay_s_before_start = int(eg.enterbox(msg = msg, title = title, default = 5, strip = True))
+
+time.sleep(delay_s_before_start)
+
+for i in range(num_measurements):
+	print(dmm.measure_voltage())
