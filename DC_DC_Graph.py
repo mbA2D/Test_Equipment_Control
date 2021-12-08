942128bd9284b184f6c823ff76f112c8fdc3000b
mbA2D <ottawaelectronics2016@gmail.com>
2021-12-07 22:58:50 -0800

    fixed some bugs with DC DC Graph

--- DC_DC_Graph.py
+++ DC_DC_Graph.py
@@ -7,7 +7,7 @@ import os
 
 def calc_eff(df):
 	df['p_in'] = df['v_in'] * df['i_in']
-	df['p_out'] = df['v_out'] * df['i_out']
+	df['p_out'] = df['v_out'] * abs(df['i_out'])
 	df['eff'] = df['p_out'] / df['p_in']
 
 def plot_eff(df, test_name, save_filepath = ''):
@@ -17,14 +17,14 @@ def plot_eff(df, test_name, save_filepath = ''):
 	fig, ax = plt.subplots()
 	fig.set_size_inches(12,10)
 	
-	set_votlages = pd.unique(df['v_in_set'])
+	set_voltages = pd.unique(df['v_in_set'])
 	
 	num_voltages = len(set_voltages)
 	cm = plt.get_cmap('Set1')
 	ax.set_prop_cycle('color', [cm(1.*i/num_voltages) for i in range(num_voltages)])
 
-	for voltage in voltages:
-		df_mask = df[df['v_set_in'] == voltage]
+	for voltage in set_voltages:
+		df_mask = df[df['v_in_set'] == voltage]
 		ax.plot('i_out', 'eff', data = df_mask, label = voltage)
 	
 	title = "{} Efficiency".format(test_name)
@@ -50,13 +50,13 @@ if __name__ == '__main__':
 	filepaths = FileIO.get_filepath()
 	
 	#Ensure a directory to store the graphs exists
-	graph_dir = FileIO.ensure_subdir_exsits_file(filepaths[0], "Graphs")
+	graph_dir = FileIO.ensure_subdir_exists_file(filepaths[0], "Graphs")
 	split_path = filepaths[0].split(" ")
 	test_name = split_path[0]
 	graph_filename = os.path.join(graph_dir, "{}_Graph".format(test_name))
 	
 	#get the input voltage for this test
-	df = pd.read_csv(filepath)
+	df = pd.read_csv(filepaths)
 	
 	calc_eff(df)
 	plot_eff(df, test_name, graph_filename)
