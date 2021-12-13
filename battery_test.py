6e2e67557e587e333f003ec7d816b2575f221186
Jerome <jerome@matician.com>
2021-12-13 14:02:57 -0800

    Got live pyqtgraphs working

--- battery_test.py
+++ battery_test.py
@@ -1,10 +1,14 @@
 #Program to run the charge discharge control in different processes for each channel.
 
-from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget
+from PyQt6.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel, QVBoxLayout, QHBoxLayout, QWidget, QGridLayout
+from PyQt6 import QtCore
+from pyqtgraph import PlotWidget, plot
+import pyqtgraph as pg
 from multiprocessing import Process, Queue, Event
 from functools import partial
 import queue #queue module required for exception handling of multiprocessing.Queue
 import traceback
+from random import randint
 
 from BATT_HIL import fet_board_management as fbm
 import charge_discharge_control as cdc
@@ -12,11 +16,19 @@ import equipment as eq
 import easygui as eg
 import time
 
+class LivePlot:
+	def __init__(self):
+		self.x = list(range(100))  # 100 time points
+		self.y = [randint(0,100) for _ in range(100)]  # 100 data points
+		self.y2 = [randint(0,100) for _ in range(100)]
+		self.data_line = None
+		self.data_line2 = None
+
 class MainTestWindow(QMainWindow):
 	def __init__(self):
 		super().__init__()
 		
-		self.num_battery_channels = 3
+		self.num_battery_channels = 16
 		self.dict_for_event_and_queue = {}
 
 		#Connected Equipment
@@ -27,10 +39,11 @@ class MainTestWindow(QMainWindow):
 		self.dmm_i_ch_list = [None for i in range(self.num_battery_channels)]
 		self.res_ids_dict_list = [None for i in range(self.num_battery_channels)]
 		self.mp_process_list = [None for i in range(self.num_battery_channels)]
+		self.plot_list = [LivePlot() for i in range(self.num_battery_channels)]
 		
 		
 		self.setWindowTitle("Battery Tester App")
-		central_layout = QVBoxLayout()
+		central_layout = QGridLayout()
 		
 		
 		#Create a button at the top to connect multi-channel equipment
@@ -46,6 +59,7 @@ class MainTestWindow(QMainWindow):
 		self.button_start_test_list = [QPushButton("Start Test") for i in range(self.num_battery_channels)]
 		self.data_from_ch_queue_list = [Queue() for i in range(self.num_battery_channels)]
 		self.data_dict_list = [dict() for i in range(self.num_battery_channels)]
+		self.ch_graph_widget = [pg.PlotWidget(background='w') for i in range(self.num_battery_channels)]
 		
 		for ch_num in range(self.num_battery_channels):
 			
@@ -58,18 +72,46 @@ class MainTestWindow(QMainWindow):
 			#setting up a widget and layout for each channel
 			ch_layout = QHBoxLayout()
 			ch_layout.addWidget(self.data_label_list[ch_num])
+			ch_layout.addWidget(self.ch_graph_widget[ch_num])
+			self.plot_list[ch_num].data_line = self.ch_graph_widget[ch_num].plot(self.plot_list[ch_num].x, self.plot_list[ch_num].y, pen=pg.mkPen('r'))
+			self.plot_list[ch_num].data_line2 = self.ch_graph_widget[ch_num].plot(self.plot_list[ch_num].x, self.plot_list[ch_num].y2, pen=pg.mkPen('b'))
+
+
 			ch_layout.addWidget(self.button_assign_eq_list[ch_num])
 			ch_layout.addWidget(self.button_start_test_list[ch_num])
 			
 			ch_widget = QWidget()
 			ch_widget.setLayout(ch_layout)
 			
-			central_layout.addWidget(ch_widget)
+			central_layout.addWidget(ch_widget, (ch_num % 8 + 1), (0 if ch_num < 8 else 1))
 			self.batt_ch_list[ch_num] = cdc.BatteryChannel()
 		
 		central_widget = QWidget()
 		central_widget.setLayout(central_layout)
 		self.setCentralWidget(central_widget)
+
+		self.timer = QtCore.QTimer()
+		self.timer.setInterval(50)
+		self.timer.timeout.connect(self.update_plot_data)
+		self.timer.start()
+
+	def update_plot_data(self):
+		for ch_num in range(self.num_battery_channels):
+			self.plot_list[ch_num].x = self.plot_list[ch_num].x[1:]
+			self.plot_list[ch_num].x.append(self.plot_list[ch_num].x[-1] + 1)
+			
+			self.plot_list[ch_num].y = self.plot_list[ch_num].y[1:]
+			self.plot_list[ch_num].y2 = self.plot_list[ch_num].y2[1:]
+			
+			try:
+				self.plot_list[ch_num].y.append(randint(0,100))
+				self.plot_list[ch_num].y2.append(randint(0,100))
+			except KeyError:
+				self.plot_list[ch_num].y.append(0)
+				pass
+
+			self.plot_list[ch_num].data_line.setData(self.plot_list[ch_num].x, self.plot_list[ch_num].y)
+			self.plot_list[ch_num].data_line2.setData(self.plot_list[ch_num].x, self.plot_list[ch_num].y2)
 		
 		
 	def update_loop(self):
