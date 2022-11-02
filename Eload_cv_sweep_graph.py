bcdd3ba1c984d8305d8732a83d82b7d6a85efa3e
mbA2D <ottawaelectronics2016@gmail.com>
2022-11-02 00:45:25 -0400

    Graphs looking ok for now.

--- Eload_cv_sweep_graph.py
+++ Eload_cv_sweep_graph.py
@@ -10,36 +10,42 @@ def calc_power(df):
 	df['power'] = df['voltage'] * df['current_pos']
 
 def plot_power(df, test_name, save_filepath = ''):
-	if df.size == 0:
-		return
-	
-	fig, ax = plt.subplots()
-	fig.set_size_inches(12,10)
-	ax2 = ax.twinx()
+    if df.size == 0:
+        return
 
-	ax.plot('i_out_pos', 'voltage', data = df, label = "Voltage (V)")
-	ax.plot('i_out_pos', 'power', data = df, label = "Power (W)")
-	
-	title = "{} Power".format(test_name)
-	
-	fig.suptitle(title)
-	ax.set_ylabel('Voltage (V)')
-	ax2.set_ylabel('Power (W)')
-	ax.set_xlabel('Current (A)')
-	
-	ax.grid(b=True, axis='both')
-	
-	#Add a point at the MPP
-	mpp_p = max(df['power'])
-	mpp_i = 
-	mpp_v = 
-	
-	
-	#save the file if specified
-	if(save_filepath != ''):
-		plt.savefig(os.path.splitext(save_filepath)[0])
-	
-	plt.show()
+    fig, ax = plt.subplots()
+    fig.set_size_inches(12,10)
+    ax2 = ax.twinx()
+
+    ax.plot('current_pos', 'voltage', '.-b', data = df, label = "Voltage (V)")
+    ax2.plot('current_pos', 'power', '.-r', data = df, label = "Power (W)")
+
+    title = "{} Power".format(test_name)
+
+    fig.suptitle(title)
+    ax.set_ylabel('Voltage (V)')
+    ax2.set_ylabel('Power (W)')
+    ax.set_xlabel('Current (A)')
+
+    ax.grid(b=True, axis='both')
+
+    #Add a point at the MPP and lines for mpp_v, mpp_i
+    mpp_index = df['power'].argmax()
+    mpp_p = df['power'].iat[mpp_index]
+    mpp_i = df['current_pos'].iat[mpp_index]
+    mpp_v = df['voltage'].iat[mpp_index]
+
+    ax.hlines(y = mpp_v, xmin = min(df['current_pos']), xmax = max(df['current_pos']), color = 'g', label = 'MPP Voltage: {}V'.format(round(mpp_v, 2)))
+    ax.vlines(x = mpp_i, ymin = min(df['voltage']), ymax = max(df['voltage']), color = 'g', label = 'MPP Current: {}A'.format(round(mpp_i, 2)))
+    ax.plot(mpp_i, mpp_v, 'o', color = 'r', label = 'MPP Power: {}W'.format(round(mpp_p, 2)))
+    
+    fig.legend(loc = 'upper left')
+    
+    #save the file if specified
+    if(save_filepath != ''):
+        plt.savefig(os.path.splitext(save_filepath)[0])
+
+    plt.show()
 	
 
 def create_graphs(filepaths = None, save_dir = None):
