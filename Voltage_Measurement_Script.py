592116788d9048d68157c53dfcda2dbcec6ee563
mbA2D <ottawaelectronics2016@gmail.com>
2023-02-05 18:25:24 -0500

    Update Voltage_Measurement_Script.py

--- Voltage_Measurement_Script.py
+++ Voltage_Measurement_Script.py
@@ -6,7 +6,12 @@ import easygui as eg
 
 def main():
 	dmm = eq.dmms.choose_dmm()[1]
-
+    
+    choices = ['voltage', 'current', 'temperature']
+    msg = "What do you want to measure?"
+	title = "Measurement Type"
+	measurement_type = int(eg.choicebox(msg = msg, title = title, choices = choices))
+    
 	msg = "How Many Measurements to perform?"
 	title = "Number of Measurements"
 	num_measurements = int(eg.enterbox(msg = msg, title = title, default = 100, strip = True))
@@ -18,8 +23,12 @@ def main():
 	time.sleep(delay_s_before_start)
 
 	for i in range(num_measurements):
-		print(dmm.measure_voltage())
-
+		if measurement_type == 'voltage':
+            print(dmm.measure_voltage())
+        elif measurement_type == 'current':
+            print(dmm.measure_current())
+        elif measurement_type == 'temperature':
+            print(dmm.measure_temperature())
 
 if __name__ == '__main__':
 	main()
